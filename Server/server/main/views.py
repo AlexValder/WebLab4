c31459393415a8c13c2ee61567c70516442fa496
Артур Мамедов <arturmamedov1212@gmail.com>
2020-11-25 04:19:15 +0200

    'Sign Up' and 'Sign In' implemented (but now there's no restrictions on alowed pages.)

--- Server/server/main/views.py
+++ Server/server/main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.core.handlers.wsgi import WSGIRequest
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .forms import CreateUserForm
 
 
@@ -15,10 +16,21 @@ def user(request: WSGIRequest):
 
 def auth(request: WSGIRequest):
     form = CreateUserForm()
+    print(request.POST)
     if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        print(f'Form is valid: {form.is_valid()}')
-        if form.is_valid():
-            form.save()
+        if 'email' in request.POST:
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                user = form.cleaned_data.get('username')
+                form.save()
+                return render(request, 'main/auth_success.html', {'username':user, 'up_or_in': 'Signed UP'})
+        else:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return render(request, 'main/auth_success.html', {'username':user, 'up_or_in': 'Signed IN'})
+        
     context = {'form': form}
     return render(request, 'main/auth.html', context)
