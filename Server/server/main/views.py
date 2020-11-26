e8806e3bf1ef76e6bb955778ffb34c160d64db63
Артур Мамедов <arturmamedov1212@gmail.com>
2020-11-26 04:14:51 +0200

    Unauthorized users don't have any acces to the main page any more. Further on the main page the list of anime appear.

--- Server/server/main/views.py
+++ Server/server/main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import redirect, render
 from django.core.handlers.wsgi import WSGIRequest
-from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from .forms import CreateUserForm
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='/auth')
 def index(request: WSGIRequest):
     return render(request, 'main/main.html')
 
@@ -16,6 +16,7 @@ def user(request: WSGIRequest):
 
 def auth(request: WSGIRequest):
     form = CreateUserForm()
+    context = dict()
     print(request.POST)
     if request.method == 'POST':
         if 'email' in request.POST:
@@ -30,7 +31,12 @@ def auth(request: WSGIRequest):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return render(request, 'main/auth_success.html', {'username':user, 'up_or_in': 'Signed IN'})
-        
-    context = {'form': form}
+                return render(request, 'main/main.html')
+            context['signinerror'] = 'Wrong password or email.'
+    context['form'] = form
     return render(request, 'main/auth.html', context)
+
+
+def logoutUser(request: WSGIRequest):
+    logout(request)
+    return redirect('/auth')
